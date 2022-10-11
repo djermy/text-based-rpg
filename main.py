1c9592e6678c72cff486a681be1a61d1a2b421b8
djermy <danjermy@yahoo.com>
2022-10-12 00:10:24 +0200

    updated player class

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     name = util.user_input_name()
     gender = util.user_input_gender()
     # instantiate object
-    player = rpg.Player(name, gender, rpg.Player.max_hp)
+    player = rpg.Player(name, gender, rpg.Player.max_hp, 100, 0, 0, 5, 5, 5)
 
 if __name__ == "__main__":
     main()
